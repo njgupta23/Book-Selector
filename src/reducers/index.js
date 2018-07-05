0c66458abbef1ca7822987583ec24bb09077b2db
Neha Gupta <njgupta23@gmail.com>
2018-07-05 13:27:37 -0700

    inital commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
+//ties together all reducers to set the application state
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+    books: BooksReducer,
+    activeBook: ActiveBook
 });
 
 export default rootReducer;
